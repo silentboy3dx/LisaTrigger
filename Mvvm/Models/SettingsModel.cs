0960101eb140e9a2e5385dbb68c17155f3b6e9f2
Johnny Mast <mastjohnny@gmail.com>
2022-12-10 19:07:15 +0100

    update

--- Mvvm/Models/SettingsModel.cs
+++ Mvvm/Models/SettingsModel.cs
@@ -28,14 +28,11 @@ public partial class SettingsModel
         STATE_RUNNING = 1,
     };
 
-    [ObservableProperty] 
-    private int interval = 60;
+    [ObservableProperty] private int interval = 60;
 
-    [ObservableProperty] 
-    private bool randomize;
+    [ObservableProperty] private bool randomize;
 
-    [ObservableProperty]
-    private bool debug;
+    [ObservableProperty] private bool debug;
     
     public States state = States.STATE_OFF;
 }
